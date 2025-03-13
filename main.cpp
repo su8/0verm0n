13d4644f63e13bb6a340fad2c378a7ddd5a73feb
su8 <wifiextender2@gmail.com>
2025-03-13 10:23:46 +0300

    remove excess ()

--- main.cpp
+++ main.cpp
@@ -81,7 +81,7 @@ int main(void) {
 static void adjustHumanAndMonster(int choice, int HumanEntry) {
   unsigned short int x = 0U;
   int arr[] = { std::rand() % 20 + 10,
-    std::rand() % ((choice == 1 || choice == 2)) ? 10 + 10 : 20 + 10,
+    std::rand() % (choice == 1 || choice == 2) ? 10 + 10 : 20 + 10,
     std::rand() % (choice == 2) ? 15 : 5
   };
   static std::reference_wrapper<int> HumanVars[] = { atk, def, agi };
